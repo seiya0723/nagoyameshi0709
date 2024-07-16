a120d31679c5d94f83959e938bcc4819b7dfcb34
Seiya Asahina <seiya@asahina>
2024-07-16 18:02:23 +0900

    add

--- myproject/settings.py
+++ myproject/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    "nagoyameshi",
+    "nagoyameshi.apps.NagoyameshiConfig",
     "accounts.apps.AccountsConfig",
 
 ]
