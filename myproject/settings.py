67d34cbe94c52e6698d73f9a31d1f7cea40a06ce
Seiya Asahina <seiya@asahina>
2024-07-16 13:46:29 +0900

    add

--- myproject/settings.py
+++ myproject/settings.py
@@ -37,15 +37,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "nagoyameshi",
-    "accounts.apps.AccountsConfig",
-    #"upload",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    "nagoyameshi",
+    "accounts.apps.AccountsConfig",
+
+
 ]
 
 #↓↓↓↓↓追記↓↓↓↓↓
